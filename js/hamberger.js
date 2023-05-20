c8c597f37f152ee9eb26c8008c91d0f7448767a6
thankyoupaint <111180014+thankyoupaint@users.noreply.github.com>
2023-05-20 14:24:21 +0900

    Add files via upload

---
+++ js/hamberger.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    $("#q-button1").on("click", function(){
+        $(".a-1").slideToggle();
+    });
+});
+$(document).ready(function(){
+    $("#q-button2").on("click", function(){
+        $(".a-2").slideToggle();
+    });
+});
+$(document).ready(function(){
+    $("#q-button3").on("click", function(){
+        $(".a-3").slideToggle();
+    });
+});
+$(document).ready(function(){
+    $("#q-button4").on("click", function(){
+        $(".a-4").slideToggle();
+    });
+});
